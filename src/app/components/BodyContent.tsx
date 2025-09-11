c2ddc20e8b5118e1243dddb34efa2e14bea5abec
unknown <ahumphries@iprosystems.com>
2025-09-10 17:13:51 -0700

    Made some style changes to the image and text

--- src/app/components/BodyContent.tsx
+++ src/app/components/BodyContent.tsx
@@ -7,24 +7,16 @@ export default function BodyContent({ content }) {
         <p className="text-center">{content.content}</p>
         <button>Discover More</button>
       </section>
-      <section className="columns-2">
-        <div className="aspect-square flex justify-center items-center p-4">
-          <p className="text-2xl text-justify">
+      <div className="relative w-full h-screen mx-auto flex justify-center">
+        <div className="bg-[url(https://www.thetopvillas.com/blog/wp-content/uploads/2018/07/wine-featured.jpg)] max-w-[80%] h-full">
+          <p className="text-2xl text-justify bg-black w-1/5 pa-6 relative top-1/4 left-[2%] text-white">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
             corporis mollitia quaerat! Tempora repellat repellendus a? Iure
             doloremque consequuntur quis ad maiores accusamus alias voluptates
             provident fugiat ipsam, reprehenderit deserunt.
           </p>
         </div>
-        <div className="relative aspect-square">
-          <Image
-            className="object-cover"
-            src="https://admin.sanfelice.com/app/uploads/2023/02/slider1.jpg"
-            fill
-            alt="Vineyard Image"
-          />
-        </div>
-      </section>
+      </div>
     </>
   )
 }

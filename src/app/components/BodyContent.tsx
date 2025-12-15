91f2d64efdac79063cf19e83ec78b45fc7ed7176
unknown <ahumphries@iprosystems.com>
2025-12-15 00:49:29 -0800

    Updating layout and sections

--- src/app/components/BodyContent.tsx
+++ src/app/components/BodyContent.tsx
@@ -1,33 +1,58 @@
-"use client";
+"use client"
 import Image from "next/image"
-import { Carousel } from "react-bootstrap";
+import mapImage from "@/app/images/map.jpg"
 export default function BodyContent({ content }) {
   const instagramImages = [
+    "https://plus.unsplash.com/premium_photo-1681841594224-ad729a249113?q=80&w=754&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1553361371-9b22f78e8b1d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1534655882117-f9eff36a1574?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://plus.unsplash.com/premium_photo-1681841594224-ad729a249113?q=80&w=754&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1553361371-9b22f78e8b1d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1534655882117-f9eff36a1574?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://plus.unsplash.com/premium_photo-1681841594224-ad729a249113?q=80&w=754&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1553361371-9b22f78e8b1d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1534655882117-f9eff36a1574?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://plus.unsplash.com/premium_photo-1681841594224-ad729a249113?q=80&w=754&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://images.unsplash.com/photo-1553361371-9b22f78e8b1d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://images.unsplash.com/photo-1534655882117-f9eff36a1574?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   ]
   return (
     <>
-      <section className="p-5">
-        <section className="my-20 relative flex w-full">
-          <div className="w-1/2">
-            <p className="text-4xl">{content.content}</p>
-            <button className="underline mt-10">Read More</button>
+      <section>
+        <section className="my-20">
+          <div
+            className=" p-5 bg-cover bg-center"
+            style={{ backgroundImage: `url('${mapImage.src}')` }}
+          >
+            <div className="bg-black/30 w-1/2 p-3">
+              <p className="text-2xl leading-13">{content.content}</p>
+              <button className="underline mt-10">Read More</button>
+            </div>
           </div>
         </section>
         <section>
           <div className="relative left-20 z-2 w-[30%] top-8">
             <h1 className="text-7xl font-bold">Instagram</h1>
           </div>
-          <Carousel slide>
+
+          <div className="carousel rounded-box">
             {instagramImages.map((image, index) => (
-              <Carousel.Item key={index} className="relative h-[500px] w-full">
-                <Image src={image} fill className="object-cover" alt="Wine Image" />
-              </Carousel.Item>
+              <a
+                className="carousel-item "
+                key={index}
+                href="http://www.instagram.com"
+              >
+                <Image
+                  width={450}
+                  height={450}
+                  className="object-cover"
+                  src={image}
+                  alt="Winery Instagram post"
+                />
+              </a>
             ))}
-          </Carousel>
-          <section className="p-20">
+          </div>
+          <section className="my-5 p-2 bg-black/30 w-1/2 mx-auto">
             <p className="text-7xl text-center">Schedule a tour or tasting</p>
           </section>
         </section>
